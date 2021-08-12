12dcf3d3408ddced5d3a86b81b9aaf59f542173c
SylMoreno <silvia.moreno@wizeline.com>
2021-08-12 13:34:14 -0500

    updates and fixes

--- pom/pages/projectsPage.js
+++ pom/pages/projectsPage.js
@@ -1,24 +1,23 @@
 import { Selector, t } from 'testcafe'
 import { PROJECT } from '../data/Constants'
+import sidebarOptions from './sidebarOptions'
 
 class projectsPage{
     constructor(){
-        this.plusProjectIcon = Selector('button[aria-label="Add Project"]')
         this.projectName = Selector('input[id="edit_project_modal_field_name"]')
         this.colorList = Selector('button[class="color_dropdown_toggle"]')
         // Delete method selectors
-        this.projectLabel = Selector('.item_table')
         this.projectLabelOptions = Selector('td[class="menu"]')
         this.deleteProjectOption = Selector('#menu_delete_text')
         this.confirmDeleteButton = Selector('buttpn[class="ist_button_red"]')
     }
 
     async deleteProjects(projectLabel){
-        let totalProjects = this.projectLabel.count
+        let totalProjects = sidebarOptions.projectLabel.count
         for(let i = totalProjects; i >= 0; i--){
             if(totalProjects>0){
             await t
-            .click(this.projectLabelOptions)
+            .click(sidebarOptions.projectLabelOptions)
             .click(this.deleteProjectOption)
             .click(this.confirmDeleteButton)
             }
--- pom/pages/sidebarOptions.js
+++ pom/pages/sidebarOptions.js
@@ -4,6 +4,8 @@ class sidebarOptions{
     constructor(){
         this.inboxButton = Selector('#filter_inbox')
         this.todayButton = Selector('#filter_today')
+        this.plusProjectIcon = Selector('button[aria-label="Add Project"]')
+        this.projectLabel = Selector('.item_table')
     }
 }
 
