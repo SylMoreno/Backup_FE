df545f84c492e5446b55f26a55686a3806d8fda7
SylMoreno <silvia.moreno@wizeline.com>
2021-08-13 17:13:07 -0500

    Update Roles.js

--- pom/data/Roles.js
+++ pom/data/Roles.js
@@ -1,10 +1,10 @@
 import { Role } from 'testcafe'
 import { URL, CREDENTIALS } from './Constants'
-import loginPage from '../pages/LoginPage'
-import welcomePage from '../pages/WelcomePage'
+import LoginPage from '../pages/LoginPage'
+import WelcomePage from '../pages/WelcomePage'
 
 export const validUser = Role(`${URL.SUT_URL}`, async t => {
-    await t.click(welcomePage.loginButton)
-    await loginPage.submitLoginForm(CREDENTIALS.VALID_USER.MAIL, CREDENTIALS.VALID_USER.PASSWORD)
+    await t.click(WelcomePage.loginButton)
+    await LoginPage.submitLoginForm(CREDENTIALS.VALID_USER.MAIL, CREDENTIALS.VALID_USER.PASSWORD)
     await t.wait(7000)
 }, {preserveUrl: true})
